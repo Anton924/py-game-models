3cd20bf38b63407a89f078fae95d2c3c83fb0d5a
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-08-09 19:40:39 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,37 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
+from django.db.models import ForeignKey
+from django.db.models.fields import CharField, TextField, EmailField
+
+
+class Race(models.Model):
+    name = CharField(max_length=255, unique=True)
+    description = TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = CharField(max_length=255, unique=True)
+    bonus = CharField(max_length=255)
+    race = ForeignKey(Race, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"name: {self.name}"
+
+
+class Guild(models.Model):
+    name = CharField(max_length=255, unique=True)
+    description = TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = CharField(max_length=255, unique=True)
+    email = EmailField(max_length=255)
+    bio = CharField(
+        max_length=255,
+        help_text="It stores a short description provided by a user about himself/herself."
+    )
+    race = ForeignKey(Race, on_delete=models.CASCADE)
+    guild = ForeignKey(Guild, to_field="id", on_delete=models.SET_NULL, null=True) # stores an id of the guild the player is a member of
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        content = file.read()
+        data = json.loads(content)
+    for user in data:
+        Player.objects.create(
+            nickname=user,
+            email=data[user]["email"],
+            bio=data[user]["bio"],
+            race=Race.objects.get_or_create(name=data[user]["race"]["name"], description=data[user]["race"]["description"])[0],
+
+            guild = Guild.objects.get_or_create(name=data[user]["guild"]["name"], description=data[user]["guild"]["description"])[0] if data[user]["guild"] else None,
+        )
+        skills = data[user]["race"]["skills"]
+        for skill in skills:
+            Skill.objects.get_or_create(name=skill["name"], bonus=skill["bonus"], race=Race.objects.get(name=data[user]["race"]["name"]))
+
 
 
 if __name__ == "__main__":
